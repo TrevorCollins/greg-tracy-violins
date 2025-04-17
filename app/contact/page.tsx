ccdd1e83700040dbcd5487ecb5f572ae570b80d5
TrevorCollins <tcollinst1i@gmail.com>
2025-04-17 12:36:45 -0500

    Remove phone number

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,12 @@
-import Section from "@/components/Section";
+import Section from '@/components/Section';
 
 const Contact = () => {
-  return (
-    <Section header="Contact">
-      <p className="2xl">Gregory Beckmann Tracy</p>
-      <p>(913) 205-0932</p>
-      <p>gregbtracy@gmail.com</p>
-    </Section>
-  );
+	return (
+		<Section header='Contact'>
+			<p className='2xl'>Gregory Beckmann Tracy</p>
+			<p>gregbtracy@gmail.com</p>
+		</Section>
+	);
 };
 
 export default Contact;
